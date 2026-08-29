90ef2eff9eee28511d947642c77f7db7c5fcbbdd
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-08-30 01:43:54 +0530

    Auto-commit: 1431_Kids_With_the_Greatest_Number_of_Candies

---
+++ 1431_Kids_With_the_Greatest_Number_of_Candies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandies = 0;
+        for (int candy : candies) {
+            if (candy > maxCandies) {
+                maxCandies = candy;
+            }
+        }
+        
+        List<Boolean> result = new ArrayList<>(candies.length);
+        for (int candy : candies) {
+            result.add(candy + extraCandies >= maxCandies);
+        }
+        
+        return result;
+    }
+}
