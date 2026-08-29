6d945ac7a834cc7d73b12a4e51dd7b94eeb87348
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-08-30 01:44:26 +0530

    Auto-commit: 1436_Destination_City

---
+++ 1436_Destination_City.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Set<String> outgoingCities = new HashSet<>();
+        
+        for (List<String> path : paths) {
+            outgoingCities.add(path.get(0));
+        }
+        
+        for (List<String> path : paths) {
+            String candidate = path.get(1);
+            if (!outgoingCities.contains(candidate)) {
+                return candidate;
+            }
+        }
+        
+        return "";
+    }
+}
