ece117b87fbc69eab3497abeccef66e1c3778059
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-08-31 00:24:38 +0530

    Auto-commit: 482_License_Key_Formatting

---
+++ 482_License_Key_Formatting.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String licenseKeyFormatting(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (c != '-') {
+                if (count > 0 && count % k == 0) {
+                    sb.append('-');
+                }
+                sb.append(Character.toUpperCase(c));
+                count++;
+            }
+        }
+
+        return sb.reverse().toString();
+    }
+}
