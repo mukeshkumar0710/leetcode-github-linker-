abe85790f90a0039c3840f2beb7459dce673abfc
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-08-31 22:44:38 +0530

    Auto-commit: 1189_Maximum_Number_of_Balloons

---
+++ 1189_Maximum_Number_of_Balloons.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int[] counts = new int[26];
+        for (char c : text.toCharArray()) {
+            counts[c - 'a']++;
+        }
+
+        int b = counts['b' - 'a'];
+        int a = counts['a' - 'a'];
+        int l = counts['l' - 'a'] / 2;
+        int o = counts['o' - 'a'] / 2;
+        int n = counts['n' - 'a'];
+
+        return Math.min(b, Math.min(a, Math.min(l, Math.min(o, n))));
+    }
+}
