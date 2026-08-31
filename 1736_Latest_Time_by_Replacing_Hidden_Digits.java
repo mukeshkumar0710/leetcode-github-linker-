36ac0405c404d7d0bf483c1dcb2cb375e8dea1a2
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-09-01 00:29:58 +0530

    Auto-commit: 1736_Latest_Time_by_Replacing_Hidden_Digits

---
+++ 1736_Latest_Time_by_Replacing_Hidden_Digits.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String maximumTime(String time) {
+        char[] chars = time.toCharArray();
+
+        if (chars[0] == '?') {
+            if (chars[1] == '?' || chars[1] <= '3') {
+                chars[0] = '2';
+            } else {
+                chars[0] = '1';
+            }
+        }
+
+        if (chars[1] == '?') {
+            if (chars[0] == '2') {
+                chars[1] = '3';
+            } else {
+                chars[1] = '9';
+            }
+        }
+
+        if (chars[3] == '?') {
+            chars[3] = '5';
+        }
+
+        if (chars[4] == '?') {
+            chars[4] = '9';
+        }
+
+        return new String(chars);
+    }
+}
