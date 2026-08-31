62ccbd665517ba5cdfa9c10b50d43114766834d7
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-08-31 22:37:02 +0530

    Auto-commit: 1103_Distribute_Candies_to_People

---
+++ 1103_Distribute_Candies_to_People.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] distributeCandies(int candies, int num_people) {
+int[] result = new int[num_people];
+        int give = 1;
+        int i = 0;
+
+        while (candies > 0) {
+            result[i % num_people] += Math.min(candies, give);
+            candies -= give;
+            give++;
+            i++;
+        }
+
+        return result;
+    }
+}
