6319c6ce7e2c8f191c6badf890f4e1d18789eb0e
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-09-01 22:50:41 +0530

    Auto-commit: 2068_Check_Whether_Two_Strings_are_Almost_Equivalent

---
+++ 2068_Check_Whether_Two_Strings_are_Almost_Equivalent.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        int[] freq = new int[26];
+
+        for (int i = 0; i < word1.length(); i++) {
+            freq[word1.charAt(i) - 'a']++;
+            freq[word2.charAt(i) - 'a']--;
+        }
+
+        for (int count : freq) {
+            if (Math.abs(count) > 3) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
