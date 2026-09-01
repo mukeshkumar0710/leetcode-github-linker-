e2667d5a1a789a31040a57eaa488c963f774482f
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-09-01 23:09:34 +0530

    Auto-commit: 2180_Count_Integers_With_Even_Digit_Sum

---
+++ 2180_Count_Integers_With_Even_Digit_Sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countEven(int num) {
+        int count = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if (isEvenDigitSum(i)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private boolean isEvenDigitSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum % 2 == 0;
+    }
+}
