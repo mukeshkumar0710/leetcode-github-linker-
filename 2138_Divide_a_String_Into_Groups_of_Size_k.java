92091ea36c35e1354ee3fe38c0decd3dd30c0563
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-09-01 23:03:33 +0530

    Auto-commit: 2138_Divide_a_String_Into_Groups_of_Size_k

---
+++ 2138_Divide_a_String_Into_Groups_of_Size_k.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String[] divideString(String s, int k, char fill) {
+        int n = s.length();
+        int totalGroups = (n + k - 1) / k;
+        String[] result = new String[totalGroups];
+
+        for (int i = 0; i < totalGroups; i++) {
+            int start = i * k;
+            int end = Math.min(start + k, n);
+            StringBuilder sb = new StringBuilder(s.substring(start, end));
+
+            while (sb.length() < k) {
+                sb.append(fill);
+            }
+
+            result[i] = sb.toString();
+        }
+
+        return result;
+    }
+}
