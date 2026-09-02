54764b478625b94e5efe029602496ac3435eeece
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-09-02 12:01:45 +0530

    Auto-commit: 338_Counting_Bits

--- 338_Counting_Bits.java
+++ 338_Counting_Bits.java
@@ -1,8 +1,9 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] ans = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            ans[i] = ans[i >> 1] + (i & 1);
+        int[] ans= new int[n+1];
+        ans[0]=0;
+        for(int i=0; i<n+1; i++){
+            ans[i]=ans[i>>1] + (i&1);
         }
         return ans;
     }
