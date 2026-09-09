e0b4f7e7d830d000d15e83f4431013c35eabba6d
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-09-10 00:00:49 +0530

    Auto-commit: 300_Longest_Increasing_Subsequence

---
+++ 300_Longest_Increasing_Subsequence.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] tails = new int[nums.length];
+        int size = 0;
+
+        for (int x : nums) {
+            int left = 0;
+            int right = size;
+
+            // Binary search to find the insertion point of x
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (tails[mid] < x) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+
+            tails[left] = x;
+            if (left == size) {
+                size++;
+            }
+        }
+
+        return size;
+    }
+}
