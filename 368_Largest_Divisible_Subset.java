0b6cdd29de4efb814cbfc36c9f2dd0ad55322f65
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-09-10 00:13:49 +0530

    Auto-commit: 368_Largest_Divisible_Subset

---
+++ 368_Largest_Divisible_Subset.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        int[] dp = new int[n];
+        int[] parent = new int[n];
+        Arrays.fill(dp, 1);
+        Arrays.fill(parent, -1);
+
+        int maxLen = 1;
+        int maxIndex = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    parent[i] = j;
+                }
+            }
+
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                maxIndex = i;
+            }
+        }
+
+        // Reconstruct the subset from the back using parent pointers
+        List<Integer> result = new ArrayList<>();
+        int curr = maxIndex;
+        while (curr != -1) {
+            result.add(nums[curr]);
+            curr = parent[curr];
+        }
+
+        return result;
+    }
+}
