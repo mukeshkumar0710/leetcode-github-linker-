4713e3d6d9c4d601cd9844e3d56642d58303359a
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-09-09 23:48:23 +0530

    Auto-commit: 210_Course_Schedule_II

---
+++ 210_Course_Schedule_II.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>(numCourses);
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        int[] inDegree = new int[numCourses];
+
+        for (int[] pre : prerequisites) {
+            int course = pre[0];
+            int prerequisite = pre[1];
+            adj.get(prerequisite).add(course);
+            inDegree[course]++;
+        }
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int[] order = new int[numCourses];
+        int index = 0;
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            order[index++] = curr;
+
+            for (int neighbor : adj.get(curr)) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        return index == numCourses ? order : new int[0];
+    }
+}
