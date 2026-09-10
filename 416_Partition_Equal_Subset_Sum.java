71a71f24ce446fd7d699f4f66e14bba837d4240d
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-09-10 22:36:53 +0530

    Auto-commit: 416_Partition_Equal_Subset_Sum

---
+++ 416_Partition_Equal_Subset_Sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        // If the total sum is odd, it cannot be divided into two equal subsets
+        if (totalSum % 2 != 0) {
+            return false;
+        }
+
+        int target = totalSum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+
+            if (dp[target]) {
+                return true;
+            }
+        }
+
+        return dp[target];
+    }
+}
