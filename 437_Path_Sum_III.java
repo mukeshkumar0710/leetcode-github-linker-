6e8d56d67d0b09a873b4a9f21bb4b055280286a1
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-09-10 22:48:53 +0530

    Auto-commit: 437_Path_Sum_III

---
+++ 437_Path_Sum_III.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int targetSum) {
+        Map<Long, Integer> prefixSumCount = new HashMap<>();
+        prefixSumCount.put(0L, 1);
+
+        return dfs(root, 0L, targetSum, prefixSumCount);
+    }
+
+    private int dfs(TreeNode node, long currentSum, int targetSum, Map<Long, Integer> prefixSumCount) {
+        if (node == null) {
+            return 0;
+        }
+
+        currentSum += node.val;
+
+        int paths = prefixSumCount.getOrDefault(currentSum - targetSum, 0);
+
+        prefixSumCount.put(currentSum, prefixSumCount.getOrDefault(currentSum, 0) + 1);
+
+        paths += dfs(node.left, currentSum, targetSum, prefixSumCount);
+        paths += dfs(node.right, currentSum, targetSum, prefixSumCount);
+
+        prefixSumCount.put(currentSum, prefixSumCount.get(currentSum) - 1);
+
+        return paths;
+    }
+}
