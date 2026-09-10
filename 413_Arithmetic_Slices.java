07b75f2b25f546d6c266af6231e37d9a08896140
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-09-10 22:34:54 +0530

    Auto-commit: 413_Arithmetic_Slices

---
+++ 413_Arithmetic_Slices.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return 0;
+        }
+
+        int totalSlices = 0;
+        int currentSlices = 0;
+
+        for (int i = 2; i < nums.length; i++) {
+            // Check if the current triplet forms an arithmetic progression
+            if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                currentSlices += 1;
+                totalSlices += currentSlices;
+            } else {
+                currentSlices = 0;
+            }
+        }
+
+        return totalSlices;
+    }
+}
