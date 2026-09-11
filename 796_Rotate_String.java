cbdba95838dfa818de518b1bf6ac3d217a7c1b74
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-09-11 10:39:50 +0530

    Auto-commit: 796_Rotate_String

--- 796_Rotate_String.java
+++ 796_Rotate_String.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if (s.length() != goal.length()) {
+        if(s.length() != goal.length()){
             return false;
         }
-        return (s + s).contains(goal);
+        return (s+s).contains(goal);
     }
 }
