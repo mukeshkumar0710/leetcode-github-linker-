2b16eebf03e5f6a0faff10ffe479cf73e59e04d2
MUKESHKUMAR S <310624205165@eec.srmrmp.edu.in>
2026-09-18 11:04:04 +0530

    Auto-commit: 91_Decode_Ways

--- 91_Decode_Ways.java
+++ 91_Decode_Ways.java
@@ -1,30 +1,23 @@
 class Solution {
     public int numDecodings(String s) {
-        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
-            return 0;
+        int n = s.length();
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+
+        if (s.charAt(0) != '0')
+            dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+
+            int one = s.charAt(i - 1) - '0';
+
+            if (one >= 1 && one <= 9)
+                dp[i] += dp[i - 1];
+            int two = Integer.parseInt(s.substring(i - 2, i));
+            if (two >= 10 && two <= 26)
+                dp[i] += dp[i - 2];
         }
-
-        int prev2 = 1; // Represents dp[i - 2]
-        int prev1 = 1; // Represents dp[i - 1]
-
-        for (int i = 1; i < s.length(); i++) {
-            int current = 0;
-
-            // Single digit decoding (1-9)
-            if (s.charAt(i) != '0') {
-                current += prev1;
-            }
-
-            // Two-digit decoding (10-26)
-            int twoDigit = (s.charAt(i - 1) - '0') * 10 + (s.charAt(i) - '0');
-            if (twoDigit >= 10 && twoDigit <= 26) {
-                current += prev2;
-            }
-
-            prev2 = prev1;
-            prev1 = current;
-        }
-
-        return prev1;
+        return dp[n];
     }
 }
